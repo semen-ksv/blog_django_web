44e9f1759b5c43c3690861f5591085080d0819ab
semen-ksv <ksvsem@gmail.com>
2019-12-09 23:40:58 +0200

    'Create base and index html pages for template'

--- my_blog/views.py
+++ my_blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 def blog_posts_list(request):
-    if request.method == 'GET':
-        return render(request, 'my_blog/blog_post_list.html')
+    """Create main page in blog page"""
+    # if request.method == 'GET':
+    my_name = "Sem"
+    return render(request, 'my_blog/blog_post_list.html', context={'name': my_name})
