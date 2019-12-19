daa28dd76128caee4f5edf2b78e750f386b2c181
semen-ksv <ksvsem@gmail.com>
2019-12-19 21:17:32 +0200

    'add add user images and possibility change profile'

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -9,3 +10,12 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} profile'
 
+    def save(self, *args, **kwargs):
+        """resize picture size"""
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 150 or img. width > 150:
+            output_size = (150, 150)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
