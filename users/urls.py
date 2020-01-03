d9f62d4eceb9a31a33bf49139a05da16508b1aaa
semen-ksv <ksvsem@gmail.com>
2020-01-03 22:22:41 +0200

    'add possibility reset password'

--- users/urls.py
+++ users/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
-    # path('password_reset/', auth_views.PasswordResetView.as_view(template_nama='user/password_reset.html'), name='reset'),
-    # path('password_reset/done', auth_views.PasswordResetDoneView.as_view(template_nama='user/password_reset_done.html'), name='reset_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='user/password_reset.html'), name='reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='user/password_reset_done.html'), name='reset_done'),
 ]
