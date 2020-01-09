527d6ac98ba6add03425b9b28264af14a3d805c9
semen-ksv <ksvsem@gmail.com>
2020-01-09 22:31:28 +0200

    'add reset password procedure'

--- users/urls.py
+++ users/urls.py
@@ -11,5 +11,10 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='user/password_reset.html'), name='reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='user/password_reset_done.html'), name='reset_done'),
+        template_name='user/password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='user/password_reset_complete.html'), name='password_reset_complete'),
+
 ]
