1d3036efa417cd947a2d2163b40c96aabab3cf52
semen-ksv <ksvsem@gmail.com>
2020-01-16 20:25:50 +0200

    'add image in Post and Profile Admin panel'

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Tag, Post, Comment
 
 admin.site.register(Tag)
@@ -19,7 +21,7 @@ class PostAdmin(admin.ModelAdmin):
     #     'tags',
     # )
     # inlines = [InLineTag]
-    list_display = ('title', 'author', 'date_posted',)
+    list_display = ('title', 'author', 'date_posted', 'get_image')
     list_filter = ('title',)
     search_fields = ('title', 'body',)
     fieldsets = (
@@ -35,7 +37,9 @@ class PostAdmin(admin.ModelAdmin):
             )
             }),
         )
-
+    def get_image(self, obj):
+        if obj.post_img:
+            return mark_safe(f'<img src={obj.post_img.url} width="40" height="40">')
 
 
 admin.site.register(Post, PostAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Profile
 
-admin.site.register(Profile)
+# admin.site.register(Profile)
 
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="40" height="40">')
+
+    get_image.short_description = 'Profile image'
