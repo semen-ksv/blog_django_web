44ac3b0d7efc015aeb47c8709a2844b54fc7084a
semen-ksv <ksvsem@gmail.com>
2020-06-13 20:28:56 +0300

    set up admin post form

--- my_blog/admin.py
+++ my_blog/admin.py
@@ -2,16 +2,11 @@ from ckeditor_uploader.widgets import CKEditorUploadingWidget
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 from django import forms
-from ckeditor.widgets import CKEditorWidget
 from .models import Tag, Post, Comment
 
 admin.site.register(Tag)
 admin.site.register(Comment)
 
-# @admin.register(Post)
-# class InLineTag(admin.TabularInline):
-#     model = Tag
-#     extra = 1
 
 class PostAdminForm(forms.ModelForm):
     body = forms.CharField(widget=CKEditorUploadingWidget())
@@ -21,34 +16,25 @@ class PostAdminForm(forms.ModelForm):
 
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
-    # fields = (
-    #     'title',
-    #     'author',
-    #     'slug',
-    #     'body',
-    #     'date_posted',
-    #     'tags',
-    # )
-    # inlines = [InLineTag]
+    save_as = True
+    save_on_top = True
     list_display = ('title', 'author', 'date_posted', 'get_image')
     list_filter = ('title',)
     search_fields = ('title', 'body',)
-    fieldsets = (
-        (None, {
-            'fields': (
-                'title',
+    readonly_fields = ('date_posted', 'get_image')
+    fields = ('title',
                 'slug',
                 'author',
                 'body',
+                'get_image',
                 'post_img',
                 'date_posted',
-                'tags',
-            )
-            }),
-        )
+                'tags',)
+
     def get_image(self, obj):
         if obj.post_img:
-            return mark_safe(f'<img src={obj.post_img.url} width="40" height="40">')
+            return mark_safe(f'<img src={obj.post_img.url} width="50" >')
+        return '-'
 
 
 admin.site.register(Post, PostAdmin)
--- my_blog/views.py
+++ my_blog/views.py
@@ -32,7 +32,7 @@ class PostsList(ListView):
     template_name = 'my_blog/blog_post_list.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 8
+    paginate_by = 7
 
 
 
@@ -40,7 +40,7 @@ class AuthorPostsList(ListView):
     model = Post
     template_name = 'my_blog/author_post_list.html'
     context_object_name = 'posts'
-    paginate_by = 3
+    paginate_by = 7
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
